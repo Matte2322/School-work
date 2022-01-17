77dabc3a1f08113c34d86efdc5eaf0fe8c45a5dd
Matte2322 <71736367+Matte2322@users.noreply.github.com>
2022-01-17 15:28:36 -0800

    Update 2LapsRobot.c

--- 2LapsRobot.c
+++ 2LapsRobot.c
@@ -4,7 +4,7 @@ task main()
 {
 	string name = "DoorMat";
 	displayTextLine(3,"%s", name);
-	wait1Msec(2000);	// Robot waits for 2000 milliseconds before executing program
+	wait1Msec(2000); // Robot waits for 2000 milliseconds before executing program
 
 	for(int repeats = 0; repeats <= 8; repeats++) { // repeats 8 times to make 2 laps
 
@@ -14,7 +14,7 @@ task main()
 		motor[leftMotor]  = 100;
 		wait1Msec(2000);
 
-	// Turn right 90 degress for 1.4 second
+		// Turn right 90 degress for 1.4 second
 		displayTextLine(3, "Turning right");
 		motor[rightMotor] = 0;
 		motor[leftMotor] = 50;
