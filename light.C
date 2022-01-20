80211ca80953258684d39214798849530d9d32d6
Matte2322 <71736367+Matte2322@users.noreply.github.com>
2022-01-20 10:07:14 -0800

    Add files via upload
    Code in progress.

---
+++ light.C
@@ -0,0 +1,33 @@
+#pragma config(Sensor, S1,     touchSensor,              sensorTouch)
+#pragma config(Sensor, S2,     compassSensor,            sensorVirtualCompass)
+#pragma config(Sensor, S3,     lightSensor,              sensorLightActive)
+#pragma config(Sensor, S4,     sonarSensor,              sensorSONAR)
+#pragma config(Motor,  motorA,          gripperMotor,       tmotorNormal, PIDControl, encoder)
+#pragma config(Motor,  motorB,          rightMotor,         tmotorNormal, PIDControl, encoder)
+#pragma config(Motor,  motorC,          leftMotor,          tmotorNormal, PIDControl, encoder)
+
+/************************************\
+|*  Programmer: Matthew Chen        *|
+|*                                  *|
+|*  Task: Robotics Academy          *|
+|*  Robot: Buggy Bot                *|
+\************************************/
+void driveStraight(int powerlevel){
+		motor[rightMotor] = powerlevel;
+		motor[leftMotor] = powerlevel;
+}
+
+void turnRight90(int turningRight, int turningRight1){
+	motor[rightMotor] = turningRight;
+	motor[leftMotor] = turningRight1;
+}
+
+
+
+task main()
+{
+	int fullpower = 100;
+	int right = 0;
+	int right1 = 100;
+
+}
