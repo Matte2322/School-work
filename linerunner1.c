766c12b7f3f0c77d06b7cf90129a6f71c4b018f3
Matte2322 <71736367+Matte2322@users.noreply.github.com>
2022-01-27 18:22:47 -0800

    Update linerunner1.c

--- linerunner1.c
+++ linerunner1.c
@@ -6,6 +6,8 @@
 #pragma config(Motor,  motorB,          rightMotor,         tmotorNormal, PIDControl, encoder)
 #pragma config(Motor,  motorC,          leftMotor,          tmotorNormal, PIDControl, encoder)
 
+// inconsistent code
+
 void moveForward(int powerLevel) // determine powerlevel either moving forward or backward
 {
 	motor[leftMotor] = powerLevel;
@@ -16,10 +18,11 @@ task main()
 {
 	int fullPower = 100;
 	//int fullStop = 0;
-	int partialPower = 50;
+	int partialPower = 75;
 	bool foundBlack = false;
 	int lineCount = 0;
 	//int repeats = 0;
+	//bool foundDuplicate = false;
 
 	displayBigTextLine(3, "DoorMat");
 
@@ -34,29 +37,62 @@ task main()
 		if(foundBlack && SensorValue(lightSensor) >= 30)
 		{
 			foundBlack = false;
-			lineCount++;
+			lineCount = lineCount + 1;
 		}
 
 		displayTextLine(5, "line #: %d", lineCount);
 
 		if(SensorValue(lightSensor) > 30)
 		{
-			moveForward(partialPower);
+			moveForward(fullPower);
 		}
 
 		if(lineCount == 1)
 		{
-		 	--lineCount;
+			lineCount = lineCount - 1;
+			lineCount = lineCount + 2;
+
 			moveForward(-fullPower);
 			wait1Msec(1000);
 			continue;
 		}
 		else if(lineCount == 2)
 		{
-			--lineCount;
+			lineCount = lineCount + 1;
+			moveForward(fullPower);
+			wait1Msec(2000);
+			continue;
+		}
+		else if(lineCount == 4)
+		{
+			lineCount = lineCount + 1;
 			moveForward(-fullPower);
 			wait1Msec(2000);
 			continue;
+		}
+		else if(lineCount == 5)
+		{
+			lineCount = lineCount + 1;
+			moveForward(fullPower);
+			wait1Msec(2500);
+		}
+		else if(lineCount == 7)
+		{
+			lineCount = lineCount + 1;
+			moveForward(-fullPower);
+			wait1Msec(3000);
+		}
+		else if(lineCount == 12)
+		{
+			lineCount = lineCount + 1;
+			moveForward(-fullPower);
+			wait1Msec(3500);
+		}
+		else if(lineCount == 18)
+		{
+			lineCount = lineCount + 1;
+			moveForward(-fullPower);
+			wait1Msec(4300);
 		}
 	}
 }
